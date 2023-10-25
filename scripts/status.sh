e9471435232a2ded2749e8a3c2b9e50d63562aa4
devishke-orange <devishke@orangehrmlive.com>
2023-10-26 03:40:12 +0530

    Update status.sh

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,21 +1,24 @@
-RED="\e[1;31m"
+#!/bin/bash
+
+# Set required variables
 GREEN="\e[1;32m"
 YELLOW="\e[1;33m"
 ENDCOLOR="\e[0m"
 
+# Unset all variables
+cleanup() {
+    unset GREEN
+    unset YELLOW
+    unset ENDCOLOR
+}
+
 if [[ -z "/home/ec2-user/orangehrm-aws/installed" ]]
 then
-    echo -e "\n#################################################################"
-    echo "#								#"
-    echo -e "#	             ${GREEN}OrangeHRM is installed!${ENDCOLOR}			#"
-    echo "#								#"
-    echo -e "#################################################################\n"
-
+    # TO-DO add version
+    echo -e "\n${GREEN}OrangeHRM is installed!${ENDCOLOR}\n"
+    cleanup
 else
-    echo -e "\n#################################################################"
-    echo "#								#"
-    echo -e "#	         ${YELLOW}OrangeHRM is not installed!${ENDCOLOR}			#"
-    echo -e "#	Run ${GREEN}orangehrm install${ENDCOLOR} to start the installation		#"
-    echo "#								#"
-    echo -e "#################################################################\n"
+    echo -e "\n${YELLOW}OrangeHRM is not installed!${ENDCOLOR}"
+    echo -e "Run ${GREEN}orangehrm install${ENDCOLOR} to start the installation\n"
+    cleanup
 fi
