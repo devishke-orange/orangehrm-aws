e8d7e967673fff1e358d4bd2c3f95af28dbba768
devishke-orange <devishke@orangehrmlive.com>
2023-10-25 11:39:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,14 @@ BASHRC_FILE=/home/ec2-user/.bashrc
 REPO_FOLDER=/home/ec2-user/.orangehrm
 ORANGEHRM_ALIAS="alias orangehrm='sh /home/ec2-user/.orangehrm/scripts/orangehrm.sh'"
 ORANGEHRM_ALIAS_COMMENT="# This alias is related to the orangehrm command"
+WIP_ICON="🚧"
+SUCCESS_ICON="✅"
+ERROR_ICON="🔴"
 runAsRoot=false
 answer=""
 now=$(date)
 
+# Unset all variables
 cleanup() {
     unset RED
     unset GREEN
@@ -28,11 +32,15 @@ cleanup() {
     unset REPO_FOLDER
     unset ORANGEHRM_ALIAS
     unset ORANGEHRM_ALIAS_COMMENT
+    unset WIP_ICON
+    unset SUCCESS_ICON
+    unset ERROR_ICON
     unset runAsRoot
     unset answer
     unset now
 }
 
+# Write progress to log file
 write_to_log() {
     now=$(date)
     if [[ $# -eq 1 ]]; then
@@ -40,6 +48,7 @@ write_to_log() {
     fi
 }
 
+# Exiting before the installer has fully completed
 early_exit() {
     write_to_log "Installer exited early"
     cleanup
@@ -48,12 +57,14 @@ early_exit() {
     exit 0
 }
 
+# Exiting due to an error
 err_exit () {
     write_to_log "Exited with error"
     cleanup
     exit 1
 }
 
+# Check whether the user input matches y/ye/yes or n/no
 yes_no_check() {
     read -rp "> " answer
     while ! [[ $answer =~ $YES_REGEX|$NO_REGEX ]]; do
@@ -63,7 +74,7 @@ yes_no_check() {
 	done
 }
 
-# Catch CTRL-C
+# Catch CTRL-C and run early_exit function
 trap early_exit SIGINT
 
 echo -e "\n#################################################################"
@@ -76,41 +87,49 @@ echo "#   restore OrangeHRM completely via the terminal.		#"
 echo "#								#"
 echo -e "#################################################################\n"
 
-write_to_log "Installer started"
+echo -e "Start the installer? ${YELLOW}[yes/no]${ENDCOLOR}"
+yes_no_check
+if [[ $answer =~ NO_REGEX ]]; then
+    write_to_log "Installer not started"
+    early_exit
+else
+    write_to_log "Installer started"
+fi
 
-echo -e "${YELLOW}Checking pre-requisites${ENDCOLOR}...........🚧"
+echo -e "${YELLOW}Checking permissions${ENDCOLOR}..............${WIP_ICON}"
+# Check if root or sudo
+if ! [[ $(id -u) = 0 ]]; then
+    tput cuu1
+    echo -e "${RED}Checking permissions${ENDCOLOR}..............${ERROR_ICON}"
+    echo -e "The script must be run as root or with sudo!"
+    write_to_log "Script not run as root"
+    err_exit
+else
+    runAsRoot=true
+    write_to_log "Script run as root"
+    echo -e "${GREEN}Checking permissions${ENDCOLOR}..............${SUCCESS_ICON}"
+fi
+
+echo -e "${YELLOW}Checking pre-requisites${ENDCOLOR}...........${WIP_ICON}"
 # Check if git is installed
 if ! [[ $(command -v git) ]]; then
     tput cuu1
-    echo -e "${RED}Checking pre-requisites${ENDCOLOR}...........🔴"
+    echo -e "${RED}Checking pre-requisites${ENDCOLOR}...........${ERROR_ICON}"
     echo -e "{RED}git is not installed!${ENDCOLOR}"
     echo "Please install git and try running the script again"
     write_to_log "git not installed"
     err_exit
 else
     tput cuu1
-    echo -e "${GREEN}Checking pre-requisites${ENDCOLOR}...........✅"
+    echo -e "${GREEN}Checking pre-requisites${ENDCOLOR}...........${SUCCESS_ICON}"
     write_to_log "Pre-requisite check success"
 fi
 
-# Check if root or sudo
-if [[ $(id -u) = 0 ]]; then
-    runAsRoot=true
-    write_to_log "Script run as root"
-else
-    echo -e "${YELLOW}WARNING${ENDCOLOR}: Script not run as root! Continue? ${YELLOW}[yes/no]${ENDCOLOR}"
-    yes_no_check
-    if [[ $answer =~ $NO_REGEX ]]; then
-        early_exit
-    fi
-    write_to_log "Script not run as root"
-fi
-
 # Clone the repository
-echo -e "${YELLOW}Cloning the repository${ENDCOLOR}............🚧"
+echo -e "${YELLOW}Cloning the repository${ENDCOLOR}............${WIP_ICON}"
 if [[ -d $REPO_FOLDER ]]; then
     tput cuu1
-    echo -e "${RED}Cloning the repository${ENDCOLOR}............🔴"
+    echo -e "${RED}Cloning the repository${ENDCOLOR}............${ERROR_ICON}"
     # TO-DO
     echo -e "The repository already exists. If you are trying to update the command please run ${GREEN}orangehrm update-command${ENDCOLOR}."
     write_to_log "Repo already exists"
@@ -118,15 +137,15 @@ if [[ -d $REPO_FOLDER ]]; then
 else
     git clone --progress https://github.com/devishke-orange/orangehrm-aws $REPO_FOLDER &>/dev/null
     tput cuu1
-    echo -e "${GREEN}Cloning the repository${ENDCOLOR}............✅"
+    echo -e "${GREEN}Cloning the repository${ENDCOLOR}............${SUCCESS_ICON}"
     write_to_log "Repo successfully cloned"
 fi
 
 # Write the alias to .bashrc
-echo -e "${YELLOW}Writing alias to .bashrc${ENDCOLOR}..........🚧"
+echo -e "${YELLOW}Writing alias to .bashrc${ENDCOLOR}..........${WIP_ICON}"
 if [[ $(grep "${ORANGEHRM_ALIAS}" < $BASHRC_FILE) ]]; then
     tput cuu1
-    echo -e "${RED}Writing alias to .bashrc${ENDCOLOR}..........🔴"
+    echo -e "${RED}Writing alias to .bashrc${ENDCOLOR}..........${ERROR_ICON}"
     echo "The alias is already written! Please remove any references to orangehrm from /home/ec2-user/.bashrc and restart the installer"
     write_to_log "Alias already written"
     grep orangehrm < $BASHRC_FILE >> $LOG_FILE
@@ -135,23 +154,23 @@ else
     echo -e "\n\n${ORANGEHRM_ALIAS_COMMENT}" >> $BASHRC_FILE
     echo -e "${ORANGEHRM_ALIAS}" >> $BASHRC_FILE
     tput cuu1
-    echo -e "${GREEN}Writing alias to .bashrc${ENDCOLOR}..........✅"
+    echo -e "${GREEN}Writing alias to .bashrc${ENDCOLOR}..........${SUCCESS_ICON}"
     write_to_log "Alias written to .bashrc"
 fi
 
 # Copy the login-script to /etc/profile.d/
 if [[ $runAsRoot = true ]]; then
-    echo -e "${YELLOW}Creating login script${ENDCOLOR}.............🚧"
+    echo -e "${YELLOW}Creating login script${ENDCOLOR}.............${WIP_ICON}"
     if [[ -f "/etc/profile.d/login_orangehrm.sh" ]]; then
         tput cuu1
-        echo -e "${YELLOW}Creating login script${ENDCOLOR}.............🔴"
+        echo -e "${YELLOW}Creating login script${ENDCOLOR}.............${ERROR_ICON}"
         echo "The login script already exists! Please check /etc/profile.d"
         write_to_log "Login script already exists in /etc/profile.d"
         err_exit
     else
         cp "${REPO_FOLDER}/scripts/login_orangehrm.sh" /etc/profile.d/login_orangehrm.sh 2>>$LOG_FILE
         tput cuu1
-        echo -e "${GREEN}Creating login script${ENDCOLOR}.............✅"
+        echo -e "${GREEN}Creating login script${ENDCOLOR}.............${SUCCESS_ICON}"
         write_to_log "Login script successfully moved to /etc/profile.d"
     fi
 else
