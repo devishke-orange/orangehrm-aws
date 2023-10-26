290c5858c8cd72fc6968a75c65e0afdfe880b0cc
devishke-orange <devishke@orangehrmlive.com>
2023-10-26 12:29:04 +0530

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-FILE=/home/ec2-user/orangehrm-aws/installed
+OHRM_DIR=/home/ec2-user/.orangehrm
+INSTALL_FILE="${OHRM_DIR}/installed"
+BACKUP_DIR="${OHRM_DIR}/backups"
+NOW=$(date +"%Y_%m_%d_%H_%M_%S")
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+ENDCOLOR="\e[0m"
 
-if [ -f "$FILE" ]; then
-	now=$(date +"%Y_%m_%d_%H_%M_%S")
-    mkdir /home/ec2-user/.orangehrm-backups/$now
-    docker exec ohrm-db sh -c 'exec mariadb-dump --all-databases -uroot -proot' > /home/ec2-user/.orangehrm-backups/$now/ohrm-db.sql
-    docker cp -q ohrm:/var/www/html/lib/confs/Conf.php /home/ec2-user/.orangehrm-backups/$now/Conf.php
-    cp /home/ec2-user/orangehrm-aws/compose.yml /home/ec2-user/.orangehrm-backups/$now/compose.yml
-    echo "Backup created at /home/ec2-user/.orangehrm-backups/$now"
-    unset now
-else
-    echo "OrangeHRM is not installed!"
+cleanup() {
+    unset OHRM_DIR
+    unset INSTALL_FILE
+    unset BACKUP_DIR
+    unset NOW
+}
+
+if ! [[ -f $INSTALL_FILE ]]; then
+    sh "${OHRM_DIR}/scripts/status.sh"
+    exit 1
 fi
+
+echo -e "\nA backup of your database, configuration file and compose file will be created\n"
+echo -e "${YELLOW}Creating Backup${ENDCOLOR}...................🚧"
+mkdir -p "/home/ec2-user/.orangehrm/backups/${NOW}"
+docker exec ohrm-db sh -c 'exec mariadb-dump --all-databases -uroot -proot' > "${BACKUP_DIR}/${NOW}/ohrm-db.sql"
+docker cp -q ohrm:/var/www/html/lib/confs/Conf.php "${BACKUP_DIR}/${NOW}/Conf.php"
+cp "${OHRM_DIR}/compose.yml" "${BACKUP_DIR}/${NOW}/compose.yml"
+tput cuu1
+echo -e "${GREEN}Creating Backup${ENDCOLOR}...................✅"
+echo -e "Backup created at ${BACKUP_DIR}/${NOW}\n"
+
+cleanup
+exit 0
