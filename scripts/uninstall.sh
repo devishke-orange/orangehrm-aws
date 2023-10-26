437ac499ec84530901f65bd58ea7b6875b965350
devishke-orange <devishke@orangehrmlive.com>
2023-10-26 12:30:53 +0530

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# REMOVE
+# TO-DO: REMOVE
 
-FILE=/home/ec2-user/orangehrm-aws/compose.yml
+OHRM_DIR=/home/ec2-user/.orangehrm
+INSTALL_FILE="${OHRM_DIR}/installed"
 
-if [[ -f "$FILE" ]]
-then
-    docker exec ohrm-db mariadb -proot -e "drop database if exists orangehrm"
-    docker compose -f /home/ec2-user/orangehrm-aws/compose.yml down
-    docker system prune -af
-    sudo rm -r /home/ec2-user/orangehrm-aws
-else
-    echo "OrangeHRM is not installed!"
+if ! [[ -f $INSTALL_FILE ]]; then
+    sh "${OHRM_DIR}/scripts/status.sh"
+    exit 1
 fi
+
+docker exec ohrm-db mariadb -proot -e "drop database if exists orangehrm"
+docker compose -f "${OHRM_DIR}/compose.yml" down
+docker system prune -af
+rm -f "${OHRM_DIR}/installed"
+rm -rf "${OHRM_DIR}/backups/*"
+
+unset OHRM_DIR
+unset INSTALL_FILE
+
+exit 0
