4dd5369f3d3feeaefb052775c4255eb999b4371c
devishke-orange <devishke@orangehrmlive.com>
2023-10-26 12:30:31 +0530

    Update status.sh

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Set required variables
+INSTALL_FILE="/home/ec2-user/.orangehrm/installed"
 GREEN="\e[1;32m"
 YELLOW="\e[1;33m"
 ENDCOLOR="\e[0m"
 
 # Unset all variables
 cleanup() {
+    unset INSTALL_FILE
     unset GREEN
     unset YELLOW
     unset ENDCOLOR
 }
 
-# TO-DO install check
-if [[ -z "/home/ec2-user/orangehrm-aws/installed" ]]
+if [[ -f $INSTALL_FILE ]]
 then
     # TO-DO add version
     echo -e "\n${GREEN}OrangeHRM is installed!${ENDCOLOR}\n"
