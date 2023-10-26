17478d55903b605156eba1b7f7b90333386cbc67
devishke-orange <devishke@orangehrmlive.com>
2023-10-26 12:29:48 +0530

    Update orangehrm.sh

--- scripts/orangehrm.sh
+++ scripts/orangehrm.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
+SCRIPT_DIR=/home/ec2-user/.orangehrm/scripts
+HELP_FILE=/home/ec2-user/.orangehrm/assets/HELP
+
 function help() {
-    echo "Usage: orangehrm COMMAND"
-    echo "Commands:"
-    echo "  status      check whether the system is installed or not"
-    echo "  install     installs the system"
-    echo "  upgrade     upgrade the system"
-    echo "  backup      backup the system"
-    echo "  restore     restore a backup"
+    cat $HELP_FILE
 }
 
-if [[ $# -eq 0 ]]
-then
+# Give the name of the script as the argument
+ohrm_script() {
+    sh "${SCRIPT_DIR}/${1}"
+}
+
+if [[ $# -eq 0 ]]; then
+    # If no arguments
     help
-elif [[ $1 = "install" ]]
-then
-    sh /home/ec2-user/.orangehrm/scripts/install_orangehrm.sh
-elif [[ $1 = "upgrade" ]]
-then
-    sh /home/ec2-user/.orangehrm/scripts/upgrade.sh
-elif [[ $1 = "backup" ]]
-then
-    sh /home/ec2-user/.orangehrm/scripts/backup.sh
-elif [[ $1 = "restore" ]]
-then
-    sh /home/ec2-user/.orangehrm/scripts/restore.sh
-elif [[ $1 = "uninstall" ]]
-then
-    sh /home/ec2-user/.orangehrm/scripts/uninstall.sh
-elif [[ $1 = "status" ]]
-then
-    sh /home/ec2-user/.orangehrm/scripts/status.sh
+elif [[ $1 = "install" ]]; then
+    ohrm_script install_orangehrm.sh
+elif [[ $1 = "upgrade" ]]; then
+    ohrm_script upgrade.sh
+elif [[ $1 = "backup" ]]; then
+    ohrm_script backup.sh
+elif [[ $1 = "restore" ]]; then
+    ohrm_script restore.sh
+elif [[ $1 = "uninstall" ]]; then
+    # TO-DO: remove?
+    ohrm_script uninstall.sh
+elif [[ $1 = "status" ]]; then
+    ohrm_script status.sh
+elif [[ $1 = "create-login-message" ]]; then
+    ohrm_script create_login_message.sh
+elif [[ $1 = "help" ]]; then
+    help
 else
-    help
+    echo -e "\n${1} is not an orangehrm command"
+    echo -e "See 'orangehrm help'\n"
 fi
+
+unset SCRIPT_DIR
+unset HELP_FILE
+
+exit 0;
