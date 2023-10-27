2eb088e93ae1acf48fd8608a6a189c3975d2c9bd
devishke-orange <devishke@orangehrmlive.com>
2023-10-27 09:37:39 +0530

    Update check_update.sh

--- scripts/check_update.sh
+++ scripts/check_update.sh
@@ -11,13 +11,19 @@ cleanup() {
 
 if ! [[ -f $INSTALL_FILE ]]; then
     sh "${OHRM_DIR}/scripts/status.sh"
+    cleanup
     exit 1
 fi
 
 git -C $OHRM_DIR fetch --quiet
-newVersion=$(git -C $OHRM_DIR diff main origin/main)
+newVersion=$(git -C .orangehrm/ diff main origin/main | grep "+    image: orangehrm/orangehrm")
 
 if [[ -z $newVersion ]]; then
-    echo "OrangeHRM is up to date!"
-    exit 0;
+    echo -e "\nOrangeHRM is up to date!\n"
+else
+    echo "A new version of OrangeHRM is available!"
+    echo "Please run '${GREEN}orangehrm upgrade{$ENDCOLOR}' to upgrade!"
 fi
+
+cleanup
+exit 0;
