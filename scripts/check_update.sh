a6227c053679dd6b7100fd9bfd40829bc24e1c7d
devishke-orange <devishke@orangehrmlive.com>
2023-10-31 07:50:03 +0530

    Update check_update.sh

--- scripts/check_update.sh
+++ scripts/check_update.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
 OHRM_DIR=/home/ec2-user/.orangehrm
-INSTALL_FILE="${OHRM_DIR}/installed"
+INSTALL_FILE="${OHRM_DIR}/.installed"
+COMPOSE_FILE="${OHRM_DIR}/compose.yml"
 GREEN="\e[1;32m"
 ENDCOLOR="\e[0m"
+currentVersion=""
+newVersion=""
 
-cleanup() {
-    unset OHRM_DIR
-    unset INSTALL_FILE
-    unset newVersion
-    unset GREEN
-    unset ENDCOLOR
+check_installed() {
+    if ! [[ -f $INSTALL_FILE ]]; then
+        sh "${OHRM_DIR}/scripts/status.sh"
+        cleanup
+        exit 1
+    fi
 }
 
-if ! [[ -f $INSTALL_FILE ]]; then
-    sh "${OHRM_DIR}/scripts/status.sh"
-    cleanup
-    exit 1
-fi
+get_versions() {
+    currentVersion=$(grep "    image: orangehrm/orangehrm:" ${COMPOSE_FILE} | sed 's/    image: orangehrm\/orangehrm://')
+    newVersion=$(curl --silent https://raw.githubusercontent.com/devishke-orange/orangehrm-aws/main/compose.yml | grep "    image: orangehrm/orangehrm:" | sed 's/    image: orangehrm\/orangehrm://')
+}
 
-git -C $OHRM_DIR fetch --quiet
-newVersion=$(git -C .orangehrm/ diff main origin/main | grep "+    image: orangehrm/orangehrm")
+check_update() {
+    if [[ "${currentVersion}" = "${newVersion}" ]]; then
+        echo -e "\nOrangeHRM is up to date!\n"
+    else
+        echo -e "\nA new version of OrangeHRM is available!"
+        echo -e "Please run '${GREEN}orangehrm upgrade${ENDCOLOR}' to upgrade!\n"
+    fi
+}
 
-if [[ -z $newVersion ]]; then
-    echo -e "\nOrangeHRM is up to date!\n"
-else
-    echo "A new version of OrangeHRM is available!"
-    echo -e "Please run '${GREEN}orangehrm upgrade{$ENDCOLOR}' to upgrade!"
-fi
+cleanup() {
+    unset INSTALL_FILE
+    unset GREEN
+    unset ENDCOLOR
+    unset currentVersion
+    unset newVersion
+    unset get_versions
+    unset check_update
+}
+
+check_installed
+get_versions
+check_update
 
 cleanup
+unset cleanup
 exit 0;
